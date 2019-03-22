61e9bc44e2ca4e0d1a6cbae63311b628d8d1d44e
Zach Williams <zwilliams.dev@gmail.com>
2019-03-21 21:49:19 -0400

    Project setup and initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express'); // Backend framework
+const mongoose = require('mongoose');
+const app = express(); // Express object
+const PORT = process.env.PORT || 5000; // Port for server to run on
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/posts');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db,  { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', post);
+
+app.get('/', (req, res) => res.send('Hello world!')); // test route
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
