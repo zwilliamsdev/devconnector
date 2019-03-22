180bacfe18bb49bfda9b2902a86a689939d5ddb5
Zach Williams <zwilliams.dev@gmail.com>
2019-03-21 22:07:31 -0400

    Created user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+// Create schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+// Export module for use outside of this file,
+// set a variable named User, set the name and pass the schema in
+module.exports = User = mongoose.model('users', UserSchema);
