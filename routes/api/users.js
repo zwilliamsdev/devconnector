aef5e173d115d895b96acd7db2dcb27da13245aa
Zach Williams <zwilliams.dev@gmail.com>
2019-03-22 21:34:00 -0400

    Sign JSON webtoken with user info in payload

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // Load User model
 const User = require("../../models/User");
@@ -59,7 +61,25 @@ router.post("/login", (req, res) => {
       // Check password
       bcrypt.compare(password, user.password).then(isMatch => {
         if (isMatch) {
-          res.json({ msg: "Success" });
+          // User Matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          };
+          // Sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 28800 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
         } else {
           return res.status(400).json({ password: "Password incorrect." });
         }
